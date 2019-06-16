cffd4f81c7f8f2a9a1acbb0bef1463ed02d2200a
Mike Conley <conleym@users.noreply.github.com>
2019-06-16 14:13:50 -0400

    Use -u flag to upgrade instead of uninstalling outdated ports.

--- update_ports.sh
+++ update_ports.sh
@@ -6,7 +6,6 @@ echo "Updating MacPorts..."
 CMD="/usr/bin/sudo /opt/local/bin/port"
 
 ${CMD} selfupdate
-${CMD} upgrade outdated
-${CMD} uninstall inactive
+${CMD} upgrade -u outdated
 
 echo "Done updating MacPorts."
