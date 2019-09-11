296e3c2e973298cd1be13737b746a4f175480e3a
Mike Conley <conleym@users.noreply.github.com>
2019-09-11 16:15:22 -0400

    Add reinstall forcibly removed packages to update_texlive.sh

--- update_texlive.sh
+++ update_texlive.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 echo "Updating TeX...."
-/usr/bin/sudo /Library/TeX/texbin/tlmgr update --self --all
+# update TexLive manager, outdated packages, and reinstall removed.
+# forcibly removed packages just seem to happen from time to time, probably
+# because of errors updating.
+#
+# See https://tex.stackexchange.com/a/107328/80250
+/usr/bin/sudo /Library/TeX/texbin/tlmgr update --self --all \
+              --reinstall-forcibly-removed
 echo "Done updating TeX."
